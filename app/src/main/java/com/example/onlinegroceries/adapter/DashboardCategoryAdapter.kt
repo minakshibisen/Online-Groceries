c3f0c4ed8c3a5823df8aa984d10a629880c27d4c
Minakshi <minakshi@bridgelogicsoftware.com>
2024-05-01 18:48:22 +0530

    office commit,shop fragment,signup,login

---
+++ app/src/main/java/com/example/onlinegroceries/adapter/DashboardCategoryAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.onlinegroceries.adapter
+
+class DashboardCategoryAdapter() {
+}
---
+++ app/src/main/java/com/example/onlinegroceries/model/DashboardDataModel.kt
@@ -0,0 +1,22 @@
+package com.example.onlinegroceries.model
+
+data class DashboardDataModel(
+    val `data`: List<Data>,
+    val msg: String,
+    val result: Boolean
+) {
+    data class Data(
+        val cat_id: String,
+        val count: Int,
+        val productList: List<Product>,
+        val title: String
+    ) {
+        data class Product(
+            val _id: String,
+            val image: String,
+            val name: String,
+            val price: String,
+            val quantity: String
+        )
+    }
+}
